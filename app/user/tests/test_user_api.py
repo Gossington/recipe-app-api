0170cf7bf3e80e72f50e0ac1d478eeb1e03dd75e
James <james.goss@outlook.com>
2023-02-12 16:00:44 +1000

    Added email not found test

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -55,7 +55,7 @@ class PublicUserApiTests(TestCase):
         """Test an error is returned if password less than 5 chars"""
         payload = {
             'email': 'test@example.com',
-            'password': 'pq',
+            'password': 'pw',
             'name': 'Test name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
@@ -94,19 +94,27 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-def test_create_token_blank_password(self):
-    """Test posting a blank password returns an error."""
-    payload = {'email': 'test@example.com', 'password': ''}
-    res = self.client.post(TOKEN_URL, payload)
+    def test_create_token_email_not_found(self):
+        """Test error returned if user not found for given email."""
+        payload = {'email': 'test@example.com', 'password': 'pass123'}
+        res = self.client.post(TOKEN_URL, payload)
 
-    self.assertNotIn('token', res.data)
-    self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-def test_retrieve_user_unauthorized(self):
-    """test authentication is required for users."""
-    res = self.client.get(ME_URL)
+    def test_create_token_blank_password(self):
+        """Test posting a blank password returns an error."""
+        payload = {'email': 'test@example.com', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
 
-    self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """test authentication is required for users."""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateUserApiTests(TestCase):
